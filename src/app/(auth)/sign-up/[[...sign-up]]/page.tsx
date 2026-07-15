02061490c9d7e751a6e9d33b721c9c2b666c28fb
Moshu <asadshahid234@gmail.com>
2026-07-15 22:00:06 +0500

    fix: switch Clerk auth to standard path routing with catch-all routes and post-sign-in redirect

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-  return <SignUp routing="hash" />;
+  return <SignUp fallbackRedirectUrl="/application-board" />;
 }
